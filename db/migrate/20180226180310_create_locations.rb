272fa37970b37106767a0d19b5a316ecd035f60b
dmitrysbn <dmitry.sbn@gmail.com>
2018-02-26 13:07:21 -0500

    Add locations table

---
+++ db/migrate/20180226180310_create_locations.rb
@@ -0,0 +1,9 @@
+class CreateLocations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :locations do |t|
+      t.string :address
+      t.string :city
+      t.string :country
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226175654) do
+ActiveRecord::Schema.define(version: 20180226180310) do
+
+  create_table "locations", force: :cascade do |t|
+    t.string "address"
+    t.string "city"
+    t.string "country"
+  end
 
   create_table "parts", force: :cascade do |t|
     t.string  "name"
