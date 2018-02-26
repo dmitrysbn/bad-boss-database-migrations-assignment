cadee34eeccc86e0c9d35f1797841c3b40129eab
dmitrysbn <dmitry.sbn@gmail.com>
2018-02-26 13:31:56 -0500

    Change locations (reversible) to only track city and weather

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226181532) do
+ActiveRecord::Schema.define(version: 20180226182244) do
 
   create_table "locations", force: :cascade do |t|
-    t.string "address"
     t.string "city"
-    t.string "country"
+    t.string "weather"
   end
 
   create_table "parts", force: :cascade do |t|
