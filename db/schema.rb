d6b653e514445d29473f476ff3b175e1354e805b
dmitrysbn <dmitry.sbn@gmail.com>
2018-02-26 13:44:43 -0500

    Add (reversible) delete widgets migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226183327) do
+ActiveRecord::Schema.define(version: 20180226183758) do
 
   create_table "locations", force: :cascade do |t|
     t.string "city"
@@ -23,18 +23,6 @@ ActiveRecord::Schema.define(version: 20180226183327) do
     t.string  "manufacturer"
     t.decimal "cost"
     t.decimal "quantity"
-  end
-
-  create_table "widgets", force: :cascade do |t|
-    t.string  "first_name"
-    t.string  "last_name"
-    t.string  "dietary_restrictions"
-    t.integer "salary"
-    t.integer "number_of_kids"
-    t.string  "vulnerabilities"
-    t.string  "illnesses"
-    t.string  "medication"
-    t.string  "voting_preferences"
   end
 
 end
